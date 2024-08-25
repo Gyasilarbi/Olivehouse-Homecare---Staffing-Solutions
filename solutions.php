5973d2f0ef2482ecffbf5643a08a36db16c25d45
Gyasilarbi <gyasilarbi459@gmail.com>
2024-08-25 23:41:39 +0000

    update

---
+++ solutions.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <title>ohss.com</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/ohhss.png" rel="icon" />
+    <link href="assets/img/ohhss.png" rel="apple-touch-icon" />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link
+      href="assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link
+      href="assets/vendor/fontawesome-free/css/all.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet" />
+  </head>
+  <body>
+    <header id="header" class="header sticky-top">
+      <div class="content">
+        <div class="row">
+          <div class="column">
+            <div class="contact-info">
+              <a href="+233201166637">
+                <span>Mon-Fri: 9am to 5pm - +233201166637</span><br>
+                <span>After Hours: +233201166637</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="topbar d-flex align-items-center">
+        <div
+          class="container d-flex justify-content-center justify-content-md-between"
+        >
+          <div class="contact-info d-flex align-items-center">
+            <i class="bi bi-envelope d-flex align-items-center"
+              ><a href="olivehousehomecarestaffing@gmail.com"
+                >olivehousehomecarestaffing@gmail.com</a
+              ></i
+            >
+            <i class="bi bi-phone d-flex align-items-center ms-4"
+              ><span>+233201166637</span></i
+            >
+          </div>
+          <div class="social-links d-none d-md-flex align-items-center">
+            <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a
+              href="https://www.instagram.com/olivehousehomecare/"
+              class="instagram"
+              ><i class="bi bi-instagram"></i
+            ></a>
+            <a href="#" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+          </div>
+        </div>
+      </div>
+      <!-- End Top Bar -->
+
+      <div class="branding d-flex align-items-center">
+        <div
+          class="container position-relative d-flex align-items-center justify-content-between"
+        >
+          <a href="index.html" class="logo d-flex align-items-center me-auto">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <img
+              src="assets/img/ohhss-removebg-preview.png"
+              alt=""
+              style="
+                transform: scale(5); /* Scale the image to double its size */
+                transform-origin: 0 0;
+                margin-top: -130px;
+              "
+            />
+            <!-- <h1 class="sitename">OHSS</h1> -->
+          </a>
+
+          <nav id="navmenu" class="navmenu">
+            <ul>
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <!-- <li class="dropdown">
+                <a href="#apply"><span>Apply Now</span></a>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Staffing Solutions</a></li>
+                  <li><a href="#">Application</a></li>
+                </ul>
+              </li> -->
+              <li><a href="homecare.html">Home Care</a></li>
+              <li><a href="apply.html">Apply</a></li>
+              <!-- <li><a href="#meetourstaff">Meet Our Staff</a></li> -->
+              <li><a href="solutions.php" class="active">Staffing Solutions</a></li>
+              <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+
+          <a class="cta-btn d-none d-sm-block" href="appointment.html"
+            >Make an Appointment</a
+          >
+        </div>
+      </div>
+    </header>
+
+    <main class="main">
+      <section id="hero" class="hero section light background">
+        <img src="assets/img/home.jpg" alt="" />
+
+        <div class="container positon-relative">
+          <div class="welcome position-relative section-title">
+            <h1>"Learn About Us"</h1>
+            <!-- <div class="text-center">
+              <a href="#appointment"
+                ><button type="submit" class="btn btn-primary">
+                  Make an Appointment
+                </button></a
+              >
+            </div> -->
+          </div>
+        </div>
+      </section>
+
+      <!-- About Section -->
+      <section id="about" class="about section">
+        <div class="container">
+          <div class="row gy-4 gx-5">
+            <div
+              class="col-lg-6 position-relative align-self-start"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <img src="assets/img/about.jpg" class="img-fluid" alt="" />
+              <div class="text-center" style="margin-top: 50px">
+                <a href="#appointment"
+                  ><button type="submit" class="btn btn-primary">
+                    Apply Now
+                  </button></a
+                >
+              </div>
+            </div>
+
+            <div
+              class="col-lg-6 content"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <p>
+                Welcome to Olivehouse Homecare Staffing Solutions, a division of
+                Olivehouse Home Care, LLC. We are a growing medical staffing
+                business in Accra, Ghana with an A+ accreditation with the
+                Better Business Bureau. We offer competitive staffing rates
+                which have allowed us to become the fastest growing medical
+                staffing business in Accra, Ghana.
+              </p>
+
+              <h2 style="margin-top: 50px">
+                Olivehouse Staffing Solutions For Facilities:
+              </h2>
+              <p>
+                We help facilities find the most qualified candidates who can
+                help them reach their company goals and mission. If you are
+                looking for qualified candidates who can meet your job
+                requirements, look no further. If you are a job seeker looking
+                for a career opportunity, look no further. <br />
+                With our recruitment expertise, extensive candidate pool,
+                unparalleled customer service, low-cost rates, and staffing
+                solutions, we aim to be your staffing agency of choice. At
+                Olivehouse Staffing Solutions, we will utilize all of our
+                expertise in this industry to deliver comprehensive solutions
+                designed according to your specific needs.
+              </p>
+
+              <h2 style="margin-top: 50px">Facility Solutions</h2>
+              <p>
+                Maintaining a schedule to meet changes in census is demanding.
+                Add in a call-out or a last-minute admission and the strain on
+                your staff increases. At Heaven Sent we help with these
+                challenges by offering a workforce that is flexible and ready to
+                go.
+              </p>
+
+              <h2 style="margin-top: 50px">Give Us A Try</h2>
+              <p>
+                Today, more than ever, there is a tremendous need for medical
+                professionals. Facilities need option and nurses need
+                flexibility. If you are a medical care provider such as a
+                hospital, assisted living facility, rehabilitation hospital,
+                school district, private practice, outpatient clinic or any
+                other facility in need of skilled nursing, please consider
+                Olivehouse Staffing Solutions as your partner in delivering the
+                quality care your patients deserve!!
+              </p>
+
+              <h2 style="margin-top: 50px">Our Goal</h2>
+              <p>
+                Our goal is to provide medical facilities and medical
+                professionals with flexible staffing options. We work with
+                client facilities so they can maximize schedule flexibility at
+                affordable rates.
+              </p>
+
+              <h2 style="margin-top: 50px">Our Vision</h2>
+              <p>
+                To support medical facilities in providing quality healthcare
+                services to their residents and patients. We work to provide a
+                flexible workforce solution by having medical professionals
+                available to meet the facilities needs. Thanks in advance for
+                your consideration of Olivehouse Staffing Solutions and we would
+                love the opportunity to earn your trust!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer id="footer" class="footer light-background">
+      <div class="container footer-top">
+        <div class="row gy-4">
+          <div
+            class="col-lg-6 col-md-6 footer-about d-flex justify-content-center"
+          >
+            <a href="index.html" class="logo d-flex">
+              <img
+                src="assets/img/ohhss-removebg-preview.png"
+                alt=""
+                style="
+                  transform: scale(4); /* Scale the image to double its size */
+                  transform-origin: 0 0;
+                "
+              />
+            </a>
+          </div>
+
+          <div
+            class="col-lg-6 col-md-6 footer-links d-flex flex-column justify-content-center align-items-center text-center"
+          >
+            <div class="footer-contact pt-3">
+              <p><strong>Address:</strong> P.O.Box Co 1687</p>
+              <p class="mt-3">
+                <strong>Phone:</strong> <span>+233201166637</span>
+              </p>
+              <p>
+                <strong>Email:</strong>
+                <span>olivehousehomecarestaffing@gmail.com</span>
+              </p>
+            </div>
+
+            <div
+              class="social-links d-flex mt-4 align-items-center justify-content-center"
+            >
+              <a href=""><i class="bi bi-twitter-x"></i></a>
+              <a href=""><i class="bi bi-facebook"></i></a>
+              <a href="https://www.instagram.com/olivehousehomecare/"
+                ><i class="bi bi-instagram"></i
+              ></a>
+              <a href=""><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container copyright text-center mt-4">
+        <p>
+          © <span>Copyright</span>
+          <strong class="px-1 sitename"
+            >Olivehouse Homecare & Staffing Solutions</strong
+          >
+          <span>All Rights Reserved</span>
+        </p>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you've purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+        </div>
+      </div>
+    </footer>
+
+    <!-- Scroll Top -->
+    <a
+      href="#"
+      id="scroll-top"
+      class="scroll-top d-flex align-items-center justify-content-center"
+      ><i class="bi bi-arrow-up-short"></i
+    ></a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
